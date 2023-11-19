ffcc88dc18ea37149a4d2d730e5bf58d994d2f09
Tanisha Bisht <tanishabisht@Tanishas-MacBook-Pro-2.local>
2023-11-19 14:03:48 -0500

    change routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { SignUp, SignIn, Recommendation, Liked } from './pages'
 import ButtonAppBar from './layout/navbar'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -9,10 +9,9 @@ function App() {
         <Routes>
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
-          <Route path="/recommendation" element={<Recommendation />} />
+          <Route path="/" element={<Recommendation />} />
           <Route path="/liked" element={<Liked />} />
 
-          <Route path="/" element={<Navigate to="/recommendation" replace />} />
         </Routes>
         </ButtonAppBar>
     </Router>
