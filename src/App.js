3c7e230d4bdd214f6cf33277d179ed11ff0a7c96
Tanisha Bisht <tanishabisht@Tanishas-MBP-2.lan>
2023-11-20 16:22:17 -0500

    fix2: build fail

--- src/App.js
+++ src/App.js
@@ -4,7 +4,34 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import awsExports from './aws-exports.js';
+
+
+const awsExports = {
+  "aws_project_region": "us-east-1",
+  "aws_cognito_identity_pool_id": "us-east-1:e49f96e0-a66b-490a-a10e-e73011994172",
+  "aws_cognito_region": "us-east-1",
+  "aws_user_pools_id": "us-east-1_k8gUVvg59",
+  "aws_user_pools_web_client_id": "7399h1re77indd7u5eoaa4bu31",
+  "oauth": {},
+  "aws_cognito_username_attributes": [
+      "EMAIL"
+  ],
+  "aws_cognito_social_providers": [],
+  "aws_cognito_signup_attributes": [
+      "EMAIL"
+  ],
+  "aws_cognito_mfa_configuration": "OFF",
+  "aws_cognito_mfa_types": [
+      "SMS"
+  ],
+  "aws_cognito_password_protection_settings": {
+      "passwordPolicyMinLength": 8,
+      "passwordPolicyCharacters": []
+  },
+  "aws_cognito_verification_mechanisms": [
+      "EMAIL"
+  ]
+};
 
 
 Amplify.configure(awsExports);
