a27476e9e57c779c44e2ea5ed1b70c71023b997b
Tanisha Bisht <tanishabisht@Tanishas-MBP-2.lan>
2023-11-20 16:16:51 -0500

    fix1: build failed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import awsExports from './aws-exports';
+import awsExports from './aws-exports.js';
 
 
 Amplify.configure(awsExports);
