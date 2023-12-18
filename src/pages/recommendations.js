470b52b4208fd45a7a7bf6205b6ac41f420d098e
Tanisha Bisht <tanishabisht@Tanishas-MBP-2.home>
2023-12-17 19:22:19 -0500

    filter recommendation based on language

--- src/pages/recommendations.js
+++ src/pages/recommendations.js
@@ -3,10 +3,28 @@ import {  useEffect, useState } from 'react'
 import axios from 'axios';
 import { CustomCard } from '../components'
 import Grid from '@mui/material/Grid';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+
+const LANGUAGES = [
+    'TypeScript', 
+    'PHP',
+    'JavaScript',
+    'Go',
+    'Python',
+    'Java',
+    'HTML',
+    'CSS',
+    'Ruby',
+    'JSON',
+    'YAML'
+]
 
 const Recommendation = () => {
 
     const [recommendedRepo, setRecommendedRepo] = useState([])
+    const [languages, setLanguages] = useState([])
 
     useEffect(() => {
         axios.get('https://rwrbehkr47.execute-api.us-east-1.amazonaws.com/TestStage/api/repositories?userid=30')
@@ -19,12 +37,44 @@ const Recommendation = () => {
         })
     }, [])
 
+    const searchFilter = () => {
+        const data = {
+            language: languages
+        }
+
+        axios.post('https://rwrbehkr47.execute-api.us-east-1.amazonaws.com/searchtest/search', data)
+        .then(function (response) {
+            console.log('response: ', response);
+            setRecommendedRepo(response.data.result)
+        })
+        .catch(function (error) {
+            console.log('error: ', error);
+        })
+    }
+
 
     return (
         <div>
             <h1 style={{textAlign:'center', marginTop:'5rem'}}>Recommendations</h1>
+
+            <Autocomplete
+                multiple
+                options={LANGUAGES}
+                getOptionLabel={option => option}
+                filterSelectedOptions
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label="Filter your recommendations by languages"
+                        placeholder="languages"
+                    />
+                )}
+                onChange={(e, val) => setLanguages(val)}
+            />
+            <Button style={{marginTop: '1rem', marginBottom: '3rem'}} fullWidth variant="contained" onClick={searchFilter}>SEARCH</Button>
+                
             <Grid container spacing={2}>
-                {recommendedRepo.map(data => (
+                {recommendedRepo && recommendedRepo.map(data => (
                     <Grid item xs={6} key={data.repo_url} style={{ height: '100%' }}>
                         <CustomCard content={data} />
                     </Grid>
